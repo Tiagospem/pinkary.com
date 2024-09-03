343a2e66da652c7848f3ed09c1627011becbeb3a
Tiago <tiagospem@gmail.com>
2024-09-03 19:14:05 -0300

    update migration

--- database/migrations/2024_02_25_144346_create_likes_table.php
+++ database/migrations/2024_02_25_144346_create_likes_table.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Models\Question;
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table): void {
             $table->id();
-            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
-            $table->foreignIdFor(Question::class)->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('question_id')->constrained()->cascadeOnDelete();
 
             $table->unique(['user_id', 'question_id']);
 
